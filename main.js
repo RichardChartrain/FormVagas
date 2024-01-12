99c0a50ce23ce5cd4597411f95be68794ab28470
Richard Paul <101884441+RichardChartrain@users.noreply.github.com>
2024-01-12 15:03:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const array = [];
+/* const campoBusca = document.querySelector(); */
+const campoUf = document.querySelectorAll("#idUf");
+console.log(campoUf);
+campoUf.forEach((element) => {
+  console.log(element.value);
+});
+const campoUnidade = document.querySelectorAll("#idUnidade");
+console.log(idUnidade);
+idUnidade.forEach((element) => {
+  console.log(element.value);
+});
+const campoCargo = document.querySelectorAll("#idCargo");
+console.log(idCargo);
+idCargo.forEach((element) => {
+  console.log(element.value);
+});
